1ec685d676641cbe5c54400e9f33c6250a0c07d3
Adisteyf <94963746+adisteyf@users.noreply.github.com>
2024-05-28 19:50:27 +0300

    Delete src directory

--- src/main/java/com/fe/physic/ComponentStruct.java
+++
@@ -1,7 +0,0 @@
-package com.fe.physic;
-
-import java.util.ArrayList;
-
-public abstract class ComponentStruct {
-    public Entity ent;
-}
--- src/main/java/com/fe/physic/components/FilesScripts.java
+++
@@ -1,6 +0,0 @@
-package com.fe.physic.components;
-
-public abstract class FilesScripts {
-    public abstract void start();
-    public abstract void update(float dt);
-}
